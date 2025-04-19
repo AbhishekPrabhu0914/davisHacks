8e591f4d28fcc58070c6909116c4dc30f19d0e95
abhip1106 <abhishekprabhu04@gmail.com>
2025-04-19 11:13:53 -0700

    Initial Commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FileUploader from './FileUploader.jsx' 
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <FileUploader></FileUploader>
+    </div>
+  )
+}
+
+export default App
